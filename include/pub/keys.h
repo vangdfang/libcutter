9034f1b40e644fc32810df93232dab2ff6069120
Richard Allen <rsaxvc@gmail.com>
2010-09-18 23:30:59 -0500

    Added example curve keys. Made keys incremental.

--- include/pub/keys.h
+++ include/pub/keys.h
@@ -1,11 +1,17 @@
 #ifndef KEYS_H
 #warning Keys must be defined!
-#define MOVE_KEY_0 0ul
-#define MOVE_KEY_1 1ul
-#define MOVE_KEY_2 2ul
-#define MOVE_KEY_3 3ul
-#define LINE_KEY_0 0ul
-#define LINE_KEY_1 1ul
-#define LINE_KEY_2 2ul
-#define LINE_KEY_3 3ul
+#define MOVE_KEY_0   0ul
+#define MOVE_KEY_1   1ul
+#define MOVE_KEY_2   2ul
+#define MOVE_KEY_3   3ul
+
+#define LINE_KEY_0   4ul
+#define LINE_KEY_1   5ul
+#define LINE_KEY_2   6ul
+#define LINE_KEY_3   7ul
+
+#define CURVE_KEY_0  8ul
+#define CURVE_KEY_1  9ul
+#define CURVE_KEY_2 10ul
+#define CURVE_KEY_3 11ul
 #endif
