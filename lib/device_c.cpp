45e0909864dfccc74f582598656ff4da22da1a22
Simon Fowler <simon.fowler@anu.edu.au>
2014-12-31 10:57:36 +1100

    Revert "Resolve a timing issue in the initial setup."
    This reverts commit 377568a43602556b13af7c5a5d203741ead89033.

--- lib/device_c.cpp
+++ lib/device_c.cpp
@@ -26,7 +26,6 @@
 #include <cstdlib>
 #include <iostream>
 #include <assert.h>
-#include <unistd.h>
 #include "btea.h"
 #include "device_c.hpp"
 
@@ -41,7 +40,6 @@ struct __attribute__(( packed )) lmc_command
 
 namespace Device
 {
-     static const int DELAY = 170000;
     static const float INCHES_TO_C_UNITS = 404.0f;
     static const float C_UNITS_TO_INCHES = ( 1 / (INCHES_TO_C_UNITS) );
 
@@ -101,13 +99,11 @@ namespace Device
 
     bool C::start()
     {
-	 usleep(DELAY);
         return m_serial.p_write( cmd_start, sizeof( cmd_start ) );
     }
 
     bool C::stop()
     {
-	 usleep(DELAY);
         return m_serial.p_write( cmd_stop, sizeof( cmd_stop ) );
     }
 
